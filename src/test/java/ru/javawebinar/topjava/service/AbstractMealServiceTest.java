7848c9fcc84acbd94a9dfb9c8b0d1779fb165a57
NikoSmirnov <s9260679663@yandex.ru>
2020-03-15 20:31:57 +0300

    6 02 HW5 profile test

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+public abstract class AbstractMealServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    private MealService service;
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private MealRepository repository;
+
+    @Test
+    public void delete() throws Exception {
+        service.delete(MEAL1_ID, USER_ID);
+        Assert.assertNull(repository.get(MEAL1_ID, USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() throws Exception {
+        service.delete(1, USER_ID);
+    }
+
+
+}
